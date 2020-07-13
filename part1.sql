e7ea28cc0e510633950929c07c574b84ce65a272
bwcsemaj <bwc_semaj@live.com>
2020-07-13 10:24:44 -0400

    Missed the second part of part 1 so I fixed it

--- part1.sql
+++ part1.sql
@@ -1,4 +1,5 @@
-SELECT customerName as "Customer Name", CONCAT(employees.firstName, ' ', employees.lastName) as "Sales Rep Name"
+SELECT customerName as "Customer Name", CONCAT(employees.lastName, ", ", employees.firstName) as "Sales Rep Name"
     FROM customers
     INNER JOIN employees
     ON customers.salesRepEmployeeNumber=employees.employeeNumber
+    ORDER BY customerName ASC
